8f9a775b7bfcccdb61720d5df07842e1a82e2622
hoshmand <m.hoo1377@gmail.com>
2018-08-19 22:22:14 +0430

    کلاس دیتا بیس هنوز کامل نیست

--- app/src/main/java/hoosmand/Database.java
+++ app/src/main/java/hoosmand/Database.java
@@ -6,21 +6,46 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class Database extends SQLiteOpenHelper {
 
-    public final static int DATABASE_VERSION = 1;
-    public final static String DATABASE_NAmE = "info_db";
+    private final static int DATABASE_VERSION = 1;
+    private final static String DATABASE_NAME = "info_db";
+
+    private final static String TB_COURSE_NAME = "tb_course";
+    private final static String TB_COURSE_KEY_NAME = "name";
+    private final static String TB_COURSE_KEY_ID = "id";
+    private final static String TB_COURSE_KEY_NUMBER_OF_GROUPS = "number_of_groups";
+
+    private final static String TB_RULE_NAME = "tb_rule";
+    private final static String TB_RULE_KEY_DAY = "day";
+    private final static String TB_RULE_KEY_TIME = "time";
+    private final static String TB_RULE_KEY_RELATION = "relation";
+    private final static String TB_RULE_KEY_COURSE = "course";
+    private final static String TB_RULE_KEY_TEACHER = "teacher";
 
     public Database(Context context) {
-        super(context, DATABASE_NAmE, null, DATABASE_VERSION);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS '" + TB_COURSE_NAME + "' " +
+                "('" + TB_COURSE_KEY_NAME + "' TEXT" +
+                ", '" + TB_COURSE_KEY_ID + "' NUMERIC" +
+                ", '" + TB_COURSE_KEY_NUMBER_OF_GROUPS + "' NUMERIC" +
+                ")");
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS '" + TB_RULE_NAME + "' " +
+                "('" + TB_RULE_KEY_DAY + "' NUMERIC" +
+                ", '" + TB_RULE_KEY_TIME + "' NUMERIC" +
+                ", '" + TB_RULE_KEY_RELATION + "' NUMERIC" +
+                ", '" + TB_RULE_KEY_COURSE + "' TEXT" +
+                ", '" + TB_RULE_KEY_TEACHER + "' TEXT" +
+                ")");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS '" + TB_COURSE_NAME + "'");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS '" + TB_RULE_NAME + "'");
+        onCreate(sqLiteDatabase);
     }
 
 }
