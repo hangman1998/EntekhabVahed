8a84d55876b862bfbb3d01468ed8455d2dd1df4b
yosuef javaherian <yousefjavaherian@gmail.com>
2018-08-19 23:28:55 +0430

    commit by youesf created an activity for editing groups just need to create a custom fragment for picking timings. last push of the night

---
+++ app/src/main/java/javaherian/yousef/entekhabvahed/ActivityEditGroups.java
@@ -0,0 +1,13 @@
+package javaherian.yousef.entekhabvahed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityEditGroups extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_groups);
+    }
+}
