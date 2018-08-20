241c56d78ebf4a9720d552801986aa68e07c5288
yosuef javaherian <yousefjavaherian@gmail.com>
2018-08-21 01:01:30 +0430

    commit by yousef completed the design of edit groups created a fragment for handling timings

--- app/src/main/java/javaherian/yousef/entekhabvahed/ActivityEditGroups.java
+++ app/src/main/java/javaherian/yousef/entekhabvahed/ActivityEditGroups.java
@@ -2,8 +2,10 @@ package javaherian.yousef.entekhabvahed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ActivityEditGroups extends AppCompatActivity {
+    EditText    idEditText,teacherEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
