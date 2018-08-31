a7bea699f61ba1cd2c864458916ebf307e93a7e4
mohammad <mandyas7613@gmail.com>
2018-08-31 16:35:53 +0430

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/misc.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/javaherian/yousef/entekhabvahed/ActivityFillCourses.java
    #	app/src/main/java/javaherian/yousef/entekhabvahed/ModelRule.java
    #	app/src/main/java/motamedi/mohammad/entekhabvahed/test.java

--- app/src/main/java/heidari/mohammad/entekhabvahed/ActivityShowSchedule.java
+++ app/src/main/java/heidari/mohammad/entekhabvahed/ActivityShowSchedule.java
@@ -1,32 +1,46 @@
 package heidari.mohammad.entekhabvahed;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
+import javaherian.yousef.entekhabvahed.ModelGroup;
 import javaherian.yousef.entekhabvahed.ModelSchedule;
 import javaherian.yousef.entekhabvahed.R;
 
 import static javaherian.yousef.entekhabvahed.Global.db;
 import static javaherian.yousef.entekhabvahed.Global.i;
+import static javaherian.yousef.entekhabvahed.Global.mainProcess;
 
 public class ActivityShowSchedule extends Activity implements View.OnClickListener {
-    List<ModelSchedule> modelSchedules;
-    TableLayout tableLayout;
-    TableRow tableRowa;
+    Button before;
+    Button next;
     FragmentHomeCourse fragmentHomeCourse;
     FragmentHomeTable fragmentHomeTable;
+    ArrayList<FragmentHomeCourse> a = new ArrayList<>();
+    ArrayList<ArrayList<FragmentHomeCourse>> schedule = new ArrayList<>();
+    ArrayList<FragmentHomeTable> day = new ArrayList<>();;
+    ArrayList<String> day1 = new ArrayList<>();
+    ModelScheduleVersion1 modelScheduleVersion1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +51,27 @@ public class ActivityShowSchedule extends Activity implements View.OnClickListen
 //        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
 //        startActivity(intent);
         //modelSchedule = db.read
+        modelScheduleVersion1 = mainProcess.getSchedule(i);
         findView();
        // makeTable();
         //initView(modelSchedules.get(i));
+        initArrayDay();
+        initTableDay();
+        initTableCourse();
+        initFragmentCourseHome();
+        initListener();
 
 
 
     }
     private void findView(){
-        tableLayout = findViewById(R.id.table);
-
-
-
+        next = findViewById(R.id.next);
+        before = findViewById(R.id.before);
     }
     @Override
     public void onClick(View view) {
         if(view.getId()==R.id.before){
-            if(i==0){
+            if(i==1){
                 Toast.makeText(ActivityShowSchedule.this,"you cant go to left",Toast.LENGTH_SHORT).show();
             }
             else {
@@ -65,7 +83,7 @@ public class ActivityShowSchedule extends Activity implements View.OnClickListen
             }
         }
         else if(view.getId()==R.id.next){
-            if(i==modelSchedules.size()-1){
+            if(i==mainProcess.scheduleSize()){
                 Toast.makeText(ActivityShowSchedule.this,"you cant go to Right",Toast.LENGTH_SHORT).show();
 
             }
@@ -87,8 +105,98 @@ public class ActivityShowSchedule extends Activity implements View.OnClickListen
 //            tableLayout.addView(tableRowa);
 //        }
 
-
-    private void initView(ModelSchedule modelSchedule){
+    void initArrayDay(){
+        day1.add("شنبه");
+        day1.add("یک شنبه");
+        day1.add("دوشنبه");
+        day1.add("سه شنبه");
+        day1.add("چهارشنبه");
+        day1.add("پنچ شنبه");
 
     }
+    private void initTableDay(){
+
+        day.add((FragmentHomeTable)getFragmentManager().findFragmentById(R.id.fragment11));
+        day.add((FragmentHomeTable)getFragmentManager().findFragmentById(R.id.fragment21));
+        day.add((FragmentHomeTable)getFragmentManager().findFragmentById(R.id.fragment31));
+        day.add((FragmentHomeTable)getFragmentManager().findFragmentById(R.id.fragment41));
+        day.add((FragmentHomeTable)getFragmentManager().findFragmentById(R.id.fragment51));
+        day.add((FragmentHomeTable)getFragmentManager().findFragmentById(R.id.fragment61));
+        for(int i=0;i<6;i++){
+            day.get(i).textView.setText(day1.get(i));
+        }
+
+
+    }
+    private void initFragmentCourseHome(){
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment12));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment13));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment14));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment15));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment16));
+        schedule.add(a);
+        a.clear();
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment22));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment23));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment24));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment25));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment26));
+        schedule.add(a);
+        a.clear();
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment32));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment33));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment34));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment35));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment36));
+        schedule.add(a);
+        a.clear();
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment44));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment43));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment44));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment45));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment46));
+        schedule.add(a);
+        a.clear();
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment55));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment53));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment54));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment55));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment56));
+        schedule.add(a);
+        a.clear();
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment66));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment63));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment64));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment65));
+        a.add((FragmentHomeCourse) getFragmentManager().findFragmentById(R.id.fragment66));
+        schedule.add(a);
+        a.clear();
+
+
+
+
+
+
+    }
+    private void initTableCourse(){
+        int numeber = modelScheduleVersion1.schedule.get(ModelGroup.SATURDAY).size();
+        for(int j=0;j<6;j++) {
+            for (int i = 0; i < numeber; i++) {
+                String coursename = modelScheduleVersion1.schedule.get(j+1).get(i).courseName;
+                String teachername = modelScheduleVersion1.schedule.get(j+1).get(i).courseName;
+                String time = modelScheduleVersion1.schedule.get(j+1).get(i).timing;
+                schedule.get(j).get(i).teacher_name.setText(teachername);
+                schedule.get(j).get(i).course_name.setText(time);
+                schedule.get(j).get(i).course_name.setText(teachername);
+            }
+        }
+    }
+    void initListener(){
+        next.setOnClickListener(this);
+        before.setOnClickListener(this);
+    }
 }
+
+
+
+
--- app/src/main/java/heidari/mohammad/entekhabvahed/FragmentHomeCourse.java
+++ app/src/main/java/heidari/mohammad/entekhabvahed/FragmentHomeCourse.java
@@ -11,12 +11,16 @@ import android.widget.TextView;
 import javaherian.yousef.entekhabvahed.R;
 
 public class FragmentHomeCourse extends Fragment  {
-    TextView textView;
+    TextView course_time;
+    TextView course_name;
+    TextView teacher_name;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
         View mainView = inflater.inflate(R.layout.custom_show_coursehome, container, false);
-        textView = mainView.findViewById(R.id.a);
+        course_time = mainView.findViewById(R.id.course_time);
+        teacher_name =mainView.findViewById(R.id.teacher_name);
+        course_name= mainView.findViewById(R.id.course_name);
         return mainView;
 
     }
